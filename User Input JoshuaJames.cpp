349bf4d3f88cef2b6d5b3a03466e23731ab86354
Joshua James <89947388+JoshuJams@users.noreply.github.com>
2023-02-08 13:53:08 -0700

    Initial Upload of User Input JoshuaJames.cpp

---
+++ User Input JoshuaJames.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    string user_input;
+    cout << "Please enter your name below: \n";
+    cin >> user_input;
+    cout << "Hello, " << user_input <<"!";
+}
